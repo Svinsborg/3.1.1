c0a5f86670fbe7a132da88b97fac67fb48f7a1c9
Svinsborg <viktorov.a.m@mail.ru>
2020-04-24 06:35:43 +0300

    Initial commit

---
+++ app/src/main/java/ru/hell/a311/MainActivity.java
@@ -0,0 +1,26 @@
+package ru.hell.a311;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView lcd;
+    private Button btOne;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+
+}
